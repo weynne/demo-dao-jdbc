f62867e79927978c4b0f110a3ceeb71e5c4acc49
Weynne <weynne.lima@gmail.com>
2024-03-06 12:58:50 -0300

    DepartmentDaoJDBC insert implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -25,6 +25,11 @@ public class Program2 {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n==== TEST 3: department insert ====");
+		Department newDepartment = new Department(null, "Laptops");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
+		
 		sc.close();
 	}
 }
