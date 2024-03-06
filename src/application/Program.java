8e4908dd9f4234746424c15ed33ffc5db7e97536
Weynne <weynne.lima@gmail.com>
2024-03-06 07:50:38 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,5 +23,11 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n==== TEST 3: seller findByAll ====");
+		list = sellerDao.findAll();
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
